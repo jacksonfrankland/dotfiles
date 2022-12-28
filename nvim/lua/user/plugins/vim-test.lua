cae81d1ff30963ef80b69a5167f9afc904cd196c
Jackson Frankland <jackson.frankland@blueglue.com.au>
2022-12-29 07:27:27 +1100

    reformatting

--- nvim/lua/user/plugins/vim-test.lua
+++ nvim/lua/user/plugins/vim-test.lua
@@ -1,20 +1,21 @@
 local wk = require("which-key")
 wk.register({
     t = {
-      name = "test",
-      n = {':TestNearest<CR>', "Test nearest"},
-      f = {':TestFile<CR>', "Test file"},
-      s = {':TestSuite<CR>', "Test suite"},
-      l = {':TestLast<CR>', "Test last"},
-      v = {':TestVisit<CR>', "Test visit"},
+        name = "test",
+        n = { ':TestNearest<CR>', "Test nearest" },
+        f = { ':TestFile<CR>', "Test file" },
+        s = { ':TestSuite<CR>', "Test suite" },
+        l = { ':TestLast<CR>', "Test last" },
+        v = { ':TestVisit<CR>', "Test visit" },
     }
-  }, {prefix = "<leader>"})
+}, { prefix = "<leader>" })
 
 vim.cmd([[
-  function! FloatermStrategy(cmd)
-    execute 'silent FloatermKill'
-    execute 'FloatermNew! '.a:cmd.' |less -X'
-  endfunction
-  let g:test#custom_strategies = {'floaterm': function('FloatermStrategy')}
-  let g:test#strategy = 'floaterm'
+    function! FloatermStrategy(cmd)
+        execute 'silent FloatermKill'
+        execute 'FloatermNew! '.a:cmd.' |less -X'
+    endfunction
+
+    let g:test#custom_strategies = {'floaterm': function('FloatermStrategy')}
+    let g:test#strategy = 'floaterm'
 ]])
