5444b304e5f29978689b57f6e4867e94f557c394
Jackson Frankland <jackson.frankland@blueglue.com.au>
2023-02-23 21:22:46 +1100

    give lsp suggestions after a single character

--- nvim/lua/user/plugins/lsp.lua
+++ nvim/lua/user/plugins/lsp.lua
@@ -1,6 +1,14 @@
 local lsp = require('lsp-zero')
 lsp.preset('recommended')
+lsp.setup_nvim_cmp({
+    mappings = cmp_mappings,
+    sources = {
+        { name = 'path' },
+        { name = 'nvim_lsp', keyword_length = 1 },
+        { name = 'buffer', keyword_length = 1 },
+        { name = 'luasnip', keyword_length = 1 },
+    }
+})
 
 lsp.setup()
 vim.keymap.set('n', '<Leader>i', ':lua vim.lsp.buf.code_action()<CR>', { desc = 'Code actions' })
-
