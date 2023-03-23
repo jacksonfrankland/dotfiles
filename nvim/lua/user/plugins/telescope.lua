913a0f0733ba2d15e6e8f115eac9e28dd574086a
Jackson Frankland <jackson.frankland@blueglue.com.au>
2023-03-23 11:04:32 +1100

    Don't preview markdown files as it errors

--- nvim/lua/user/plugins/telescope.lua
+++ nvim/lua/user/plugins/telescope.lua
@@ -8,7 +8,7 @@ local new_maker = function(filepath, bufnr, opts)
     filepath = vim.fn.expand(filepath)
     vim.loop.fs_stat(filepath, function(_, stat)
         if not stat then return end
-        if stat.size > 100000 then
+        if stat.size > 100000 or filepath:match('.*%.md') then
             return
         else
             previewers.buffer_previewer_maker(filepath, bufnr, opts)
